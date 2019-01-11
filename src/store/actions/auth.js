42734d77db6a1341aa333dfe7e89bc41c41152df
Anshul889 <anshulmanaktala@gmail.com>
2019-01-11 14:56:59 +0530

    token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +25,19 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+          email: email,
+          password: password,
+          returnSecureToken: true
+        };
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyDPxetjy9qRWAAZXuk6m-ACufU40twz42U', authData)
+          .then(response => {
+              console.log(response);
+              dispatch(authSuccess(response.data));
+          })
+          .catch(err => {
+            console.log(err);
+            dispatch(authFail(err));
+          })
     };
 };
