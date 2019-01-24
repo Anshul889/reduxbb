9c885c05b9fc3d44716c255048f8b1d6d0e3200c
Anshul889 <anshulmanaktala@gmail.com>
2019-01-24 13:40:29 +0530

    token storage

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,7 @@ const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
-        {props.isAuntheticated ? <NavigationItem link="/logout">Logout</NavigationItem> : <NavigationItem link="/auth">Login</NavigationItem> }
+        {!props.isAuthenticated ? <NavigationItem link="/auth">Login</NavigationItem> : <NavigationItem link="/logout">Logout</NavigationItem> }
     </ul>
 );
 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -41,7 +41,7 @@ class Layout extends Component {
 
 const mapStateToProps = state => {
   return {
-    isAuntheticated: state.auth.token !== null
+    isAuthenticated: state.auth.token !== null
   }
 }
 
